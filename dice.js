c1ec2c2bf09e60b242a81c9c9e1aade72afcd435
clint <clint.fleetwood@gmail.com>
2020-04-10 10:04:22 -0400

    inital import

---
+++ dice.js
@@ -0,0 +1,86 @@
+import {uuid} from "./main.js";
+
+const randomIntFromInterval = (min, max)=> { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export default (s, n) => {
+    let sides = s || 20;
+    const id = uuid();
+    const diceHistory = [];
+    let name = n || sides + 'ed dice';
+
+    const roll = () =>{
+        let r = randomIntFromInterval(1,sides);
+        diceHistory.unshift(r);
+        return r;
+    }
+
+    const clearHistory = ()=>{
+        diceHistory.splice(0,diceHistory.length)
+    }
+
+    const displayElement = (parent)=>{
+        let showHistory = false;
+        let root = document.createElement('div');
+
+        let html = `
+            <div id='${id}' class='diceHolder'>
+                <div class='diceHeader'>
+                    <label>1 - ${sides}</label>
+                    <span class='remove'> X </span>
+                </div>
+                <div class='diceName'>${name}</div>
+                <button class= 'rollDiceButton'>Roll Dice</button>
+                <div class='currentRoll'></div>
+                <div class='historyHeader'><span>Dice History</span><span class='historyToggle'> v </span></div>
+                <div class="diceHistory" style='display:none'>
+                </div>
+            </div>
+        `;
+        root.innerHTML = html;
+        parent.appendChild(root);
+
+        let current = root.querySelector('.currentRoll');
+
+        let rollButton = root.querySelector('.rollDiceButton');
+        rollButton.addEventListener('click', ()=>{
+            console.log('clicked')
+            rollButton.setAttribute('disabled', true);
+            root.querySelector('.diceHistory').innerHTML = diceHistory
+                    .map(h=>`<div class='historyItem'>${h}</div>`)
+                    .join('');
+
+            let r = roll();
+
+            let ticks = 0;
+            let inter = setInterval(()=>{
+                current.innerHTML = `<span class='rolling'>${randomIntFromInterval(1, sides)}</span>`;                                    
+                if(ticks > 15){
+                    clearInterval(inter)
+                    finish()
+                }
+                ticks += 1;
+            },100);
+
+            let finish = () =>{
+                current.innerHTML = r;
+                rollButton.disabled = false;
+            }
+        })
+
+        let remove = root.querySelector('.remove');
+        remove.addEventListener('click', ()=>{
+            root.remove()
+        });
+
+        let historyToggle = root.querySelector('.historyHeader');
+        historyToggle.addEventListener('click', ()=>{
+            showHistory = showHistory ? false:true;
+            root.querySelector('.diceHistory').setAttribute('style', `display:${showHistory?'block':'none'}`)
+            root.querySelector('.historyToggle').innerHTML = showHistory ? ' ^ ' : ' v '
+        });
+    }
+
+    return {sides, diceHistory, roll, clearHistory, displayElement}
+}
---
+++ main.js
@@ -0,0 +1,55 @@
+import dice from "./dice.js"
+const cont = document.getElementById('diceContainer');
+const diceList = [];
+
+const clearError = ()=>{
+    document.getElementById('error').innerHTML = "";
+    document.getElementById('error').setAttribute('style', 'display:none');
+}
+const error = (message) =>{
+    document.getElementById('error').innerHTML = message;
+    document.getElementById('error').setAttribute('style', 'display:block');
+}
+
+const addDice = ()=>{
+    let selector = document.getElementById('diceNumber');
+    let sides = Number(selector.value);
+    if(sides == 0) {
+        error('No Number of Sides Selected');
+        return;
+    }
+    clearError()
+    let newDice = dice(sides);
+    diceList.push(newDice);
+    let parent = document.getElementById('diceContainer');
+    newDice.displayElement(parent);
+
+    selector.value = 0;
+}
+
+const addStandard = () =>{
+    clearError();
+    document.getElementById('diceNumber').value = 0;
+    diceList.splice(0, diceList.length)
+    let parent = document.getElementById('diceContainer');
+
+    diceList.push(dice(4,'D&D d4'))
+    diceList.push(dice(6,'D&D d6'))
+    diceList.push(dice(8,'D&D d8'))
+    diceList.push(dice(10,'D&D d10'))
+    diceList.push(dice(10,'D&D %'))
+    diceList.push(dice(12,'D&D d12'))
+    diceList.push(dice(20,'D&D d20'))
+
+    diceList.forEach(d=>d.displayElement(parent))
+}
+
+document.getElementById('addDice').addEventListener('click', addDice);
+document.getElementById('addStandard').addEventListener('click', addStandard);
+
+export const uuid = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+};
